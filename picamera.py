5da2bee92932104c2d6f0edec1f975fa8ff32f69
m-higashijima <boctok.ctpoba@gmail.com>
2019-10-17 17:12:36 +0900

    create class frame

---
+++ picamera.py
@@ -0,0 +1,24 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from threading import Thread
+
+class Camera(object):
+    # initialize
+    def __init__(self):
+        pass
+
+    # start thread of read frame
+    def start(self):
+        pass
+
+    # read frame
+    def update(self):
+        pass
+
+    # get frame from camera object
+    def read(self):
+        pass
+
+    # terminate thread
+    def stop(self):
+
